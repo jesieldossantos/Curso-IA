604023d5fa09fd50381cedbee3731f30c5650090
jesieldossantos <jesielsoares2@gmail.com>
2025-05-27 16:02:05 -0300

    submit

--- material/pyton/cep.py
+++ material/pyton/cep.py
@@ -1,11 +1,16 @@
-import requests
 
+import requests #biblioteca de requisição
 
-def buscar_endereco(cep):
-    url = f'https://viacep.com.br/ws/{cep}/json/'
-    response = requests.get(url)
-    if response.status_code == 200:
-        dados = response.json()
-        endereco = f"{dados['logradouro']}, {dados['bairro']}, {dados['localidade']} - {dados['uf']}"
-        print(endereco)
+def buscar_endereco(cep, campo):
+    url = f'https://viacep.com.br/ws/{cep}/json/' #rota
 
+    response = requests.get(url) #verifica dentro na rota se a requisição pode ser atendida
+
+    if response.status_code == 200: #verifica se o status code da resposta é 200
+        dados = response.json() #pega a informação json que ele devolveu
+        endereco = f"{dados['logradouro']}, {dados['bairro']}, {dados['localidade']}, {dados['uf']}" #formata o arquivo json
+        campo.value = endereco
+        campo.update()
+    else:
+        campo.value = 'CEP nao encontado'
+        campo.update()
--- material/pyton/main.py
+++ material/pyton/main.py
@@ -1,20 +1,46 @@
 from cep import buscar_endereco
 import flet as ft
-def main(page: ft.Page):
-   pass
+
 
 def main(page: ft.Page):
    page.theme_mode = ft.ThemeMode.DARK
-   page.window_width = 350
-   page.window_height = 600
+   page.window_width = 100
+   page.window_height = 200
    page.title = 'buscador de CEP'
-   page.bgcolor= ft.colors.BLUE_200
-
+   page.bgcolor= ft.Colors.BLUE_200
+   page.window.always_on_top= True
+   page.vertical_alignment= ft.MainAxisAlignment.CENTER
+   page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+   
    campo_cep= ft.TextField(
       label='Digite seu cep'
    )
       
-   page.add(campo_cep)
-    
+   icone = ft.Icon(
+      name=ft.Icons.LOCATION_ON ,
+      size=70,
+      color=ft.Colors.RED_300
+   )
+
+   campo_resposta = ft.Text(
+      value='',
+      text_align=ft.TextAlign.CENTER
+   )
+
+
+   btn_buscar = ft.ElevatedButton(
+      text='Buscar',
+      bgcolor=ft.Colors.GREEN,
+      color=ft.Colors.WHITE,
+      width=150,
+      icon=ft.Icons.SEARCH,
+      on_click=lambda evento: buscar_endereco(campo_cep.value, campo_resposta)
+   )
+   page.add(
+      icone,
+      campo_resposta,
+      campo_cep,
+      btn_buscar
+   )
 
 ft.app(main)
